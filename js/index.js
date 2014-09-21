adfb973dc740c2af05147c50e89000c5bae40a12
Valar <jakub.skalecki@gmail.com>
2014-09-21 21:08:12 +0200

    added div container

--- js/index.js
+++ js/index.js
@@ -1,24 +1,54 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+var stats, clock, container;
 
-var geometry = new THREE.SphereGeometry(3,32,32);
-var material = new THREE.MeshBasicMaterial({color: 0x00ff00, wireframe: true});
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+var camera, controls, scene, renderer;
 
-camera.position.z = 5;
 
-var render = function () {
+init();
+animate();
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+function init()
+{
+  container = $('#canvas')
 
-  renderer.render(scene, camera);
-  requestAnimationFrame(render);
-};
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(75, container.width()/container.height(), 0.1, 1000);
+  controls = new THREE.FirstPersonControls( camera );
+  controls.movementSpeed = 1;
+  controls.lookSpeed = 0.125;
+  controls.lookVertical = true;
 
-render();
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(container.width(), container.height());
+  container.append(renderer.domElement);
+
+  clock = new THREE.Clock();
+
+  stats = new Stats();
+  stats.domElement.style.position = 'absolute';
+  stats.domElement.style.top = '0px';
+  container.append( stats.domElement );
+
+  var geometry = new THREE.SphereGeometry(30,100, 100);
+  var material = new THREE.MeshBasicMaterial({color: 0x00ff00, wireframe: true});
+  var cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  camera.position.z = 40;
+}
+
+function animate() {
+
+  requestAnimationFrame( animate );
+
+  render();
+  stats.update();
+
+}
+
+function render() {
+
+  controls.update( clock.getDelta() );
+  renderer.render( scene, camera );
+
+}

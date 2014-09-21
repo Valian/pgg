2f2ebdaa7b928de0cdcd47a44c56eb349de5357d
Valar <jakub.skalecki@gmail.com>
2014-09-21 18:56:15 +0200

    added three library

--- js/index.js
+++ js/index.js
@@ -5,20 +5,20 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.SphereGeometry(3,16,8);
-var material = new THREE.LineBasicMaterial({color: 0x00ff00, linewidth: 2, wireframe: true});
+var geometry = new THREE.SphereGeometry(3,32,32);
+var material = new THREE.MeshBasicMaterial({color: 0x00ff00, wireframe: true});
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 camera.position.z = 5;
 
 var render = function () {
-  requestAnimationFrame(render);
 
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
 
   renderer.render(scene, camera);
+  requestAnimationFrame(render);
 };
 
 render();
